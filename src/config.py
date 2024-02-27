54a10f636a0f7c8041b7ed921237cb6b3495808e
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-28 00:24:12 +0100

    use env variables

--- src/config.py
+++ src/config.py
@@ -12,13 +12,13 @@ SITE_URL = f"http://{SITE_HOST}:{SITE_PORT}"
 LOGOUT_REDIRECT_URL = f"http://{SITE_HOST}:{SITE_PORT}/api/auth/logout"
 KINDE_CALLBACK_URL = f"http://{SITE_HOST}:{SITE_PORT}/api/auth/kinde_callback"
 CLIENT_ID = os.getenv("KINDE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("KINDE_SOME_SECRET")
+CLIENT_SECRET = os.getenv("KINDE_CLIENT_SECRET")
 # Quickstart copy/paste overwrite section
 
 KINDE_ISSUER_URL = os.getenv("KINDE_ISSUER_URL")
 GRANT_TYPE = GrantType.AUTHORIZATION_CODE_WITH_PKCE
-CODE_VERIFIER = "joasd923nsad09823noaguesr9u3qtewrnaio90eutgersgdsfg" # A suitably long string > 43 chars
+CODE_VERIFIER = os.getenv("SESSION_CODE_VERIFIER")
 TEMPLATES_AUTO_RELOAD = True
 SESSION_TYPE = "filesystem"
 SESSION_PERMANENT = False
-SECRET_KEY = "joasd923nsad09823noaguesr9u3qtewrnaio90eutgersgdsfgs" # Secret used for session management
+SECRET_KEY = os.getenv("SESSION_SECRET_KEY")
