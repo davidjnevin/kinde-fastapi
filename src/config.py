b6dc6011cf196625551e979685995bf9f0b4ef46
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-29 00:09:06 +0100

    remove unneccessary comments

--- src/config.py
+++ src/config.py
@@ -7,13 +7,11 @@ load_dotenv()
 SITE_HOST = os.getenv("SITE_HOST", "localhost")
 SITE_PORT = os.getenv("SITE_PORT", "8888")
 
-# Quickstart copy/paste overwrite section
 SITE_URL = f"http://{SITE_HOST}:{SITE_PORT}"
 LOGOUT_REDIRECT_URL = f"http://{SITE_HOST}:{SITE_PORT}/api/auth/logout"
 KINDE_CALLBACK_URL = f"http://{SITE_HOST}:{SITE_PORT}/api/auth/kinde_callback"
 CLIENT_ID = os.getenv("KINDE_CLIENT_ID")
 CLIENT_SECRET = os.getenv("KINDE_CLIENT_SECRET")
-# Quickstart copy/paste overwrite section
 
 KINDE_ISSUER_URL = os.getenv("KINDE_ISSUER_URL")
 GRANT_TYPE = GrantType.AUTHORIZATION_CODE_WITH_PKCE
